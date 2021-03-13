9267ab07734474f8bf404c03096075bef059955f
James <james.hallam93@gmail.com>
2021-03-13 14:35:41 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+const cors = require("cors")
+
+const PORT = 3005
+
+
+app.use(express.static("build"))
+
+app.listen(PORT, ()=>{
+    console.log(`app listening on port ${PORT}`);
+}, PORT)
+
